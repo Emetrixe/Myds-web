aed50dfaf2837f35c9b999398d71361f2690eee0
Emetrixe <78327106+Emetrixe@users.noreply.github.com>
2023-04-26 13:14:12 -0500

    Creando archivo index.html

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function showDropdown() {
+    document.getElementById("dropdown-menu").style.display = "block";
+}function hideDropdown() {
+    document.getElementById("dropdown-menu").style.display = "none";
+}
