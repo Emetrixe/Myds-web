a5cbaf3db52941ce0fc472310b6d89a1b3fd4c17
Eduardo <new60600@gmail.com>
2023-04-28 17:31:56 -0500

    Segunda avance de pagina productos grid y animaciones

--- js/script.js
+++ js/script.js
@@ -2,4 +2,19 @@ function showDropdown() {
     document.getElementById("dropdown-menu").style.display = "block";
 }function hideDropdown() {
     document.getElementById("dropdown-menu").style.display = "none";
-}
+};
+
+const gridLinks = document.querySelectorAll(".grid-link");
+
+gridLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const targetId = link.getAttribute("href");
+    const target = document.querySelector(targetId);
+    const scrollPos = target.offsetLeft;
+    document.querySelector(".container-grid").scrollTo({
+      left: scrollPos,
+      behavior: "smooth",
+    });
+  });
+});
